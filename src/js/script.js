6611e00b27d606180012a4396aee50aebad9e14e
ievgeny1992 <karbash1992@gmail.com>
2018-09-15 16:19:57 +0300

    save commit

--- src/js/script.js
+++ src/js/script.js
@@ -3,18 +3,44 @@ $( document ).ready(function() {
     var $burgerText = $('.js-burger-menu__text');
     var $header = $('.header');
     var $headerMenuWrap = $('.header-menu__wrap');
+    var $jsScroll = $('.js-scroll');
+    var $progress = $('.future-business__progress');
 
-    $burgerMenu.on('click', function() {
+    function ShowHideBurgerMenu() {
         $header.toggleClass('header__opened');
         $headerMenuWrap.toggleClass('header-menu__wrap_opened');
 
-        $(this).toggleClass('burger-menu--opened');
-        $(this).toggleClass('burger-menu--closed');
+        $burgerMenu.toggleClass('burger-menu--opened');
+        $burgerMenu.toggleClass('burger-menu--closed');
 
-        if($(this).hasClass('burger-menu--opened')){
+        if($burgerMenu.hasClass('burger-menu--opened')){
             $burgerText.text(' ');
         } else {
             $burgerText.text('Меню');
         }
+    }
+
+    $jsScroll.click(function() {
+        var element = $(this).data('link');
+
+        ShowHideBurgerMenu();
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("." + element).offset().top
+        }, 1000);
     });
+
+    $burgerMenu.on('click', function() {
+        ShowHideBurgerMenu();
+    });
+
+    $.each($progress, function() {
+        value = $(this).data('progress');
+        $(this).css('width', value + '%');
+
+        if (value == 100) {
+            $(this).addClass('future-business__progress_full')
+        }
+    });
+
 });
